929f1dfd2ee7c76a9e9908fb5831210dce4a508d
hatuey-mieses <91497061+hatuey-mieses@users.noreply.github.com>
2021-11-22 12:16:49 -0500

    Update SubjectService.java

--- discussion-board/src/main/java/com/backend/service/SubjectService.java
+++ discussion-board/src/main/java/com/backend/service/SubjectService.java
@@ -1,24 +1,30 @@
 package com.backend.service;
 
 import java.util.List;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.backend.model.Subject;
 import com.backend.model.Username;
 import com.backend.repository.SubjectRepo;
+import com.backend.repository.UserNamesRepo;
 
 @Service
 public class SubjectService {
 	
 	private SubjectRepo sr;
+	private UserNamesRepo ur;
 	
-	public SubjectService(SubjectRepo sr) {
+	@Autowired	
+	public SubjectService(SubjectRepo sr, UserNamesRepo ur) {
 		this.sr = sr;
+		this.ur = ur;
 	}
 	
 	public Subject getSubjectById(int id) {
-		return sr.findById(id);
+		return sr.findById(id).get();
 	}
 	
 	public List<Subject> getLastTenById() {
@@ -30,17 +36,31 @@ public class SubjectService {
 	}
 	
 	public List<Subject> getSubjectsByUser(Username username) {
-		
 		return sr.findByUsernameId(username.getId());
 	}
 	
+	public Subject voteSubject(int userid, int subjectid) {
+		Username u = ur.findById(userid).get();
+		Subject s = sr.findById(subjectid).get();
+		Set<Username> votes = s.getVotes();
+		votes.add(u);
+		s.setVotes(votes);
+		
+		Set<Subject> votedSubjects = u.getVoteSubjects();
+		votedSubjects.add(s);
+		
+		ur.save(u);
+		return sr.save(s);
+	}
+	
 	public Subject createSubject(Subject subject) {
-		return sr.save(subject);
+		Subject s = sr.save(subject);
+		return s;
 	}
 	
 	public boolean deleteSubject(Subject subject) {
 		boolean flag = false;
-		if(sr.findById(subject.getId())!=null ) {
+		if(sr.findById(subject.getId()).isPresent()) {
 			sr.delete(subject);
 			flag = true;
 		}
@@ -49,7 +69,7 @@ public class SubjectService {
 	
 	public Subject updateSubject(Subject subject) {
 		Subject s = null;
-		if(sr.findById(subject.getId())!=null) {
+		if(sr.findById(subject.getId()).isPresent()) {
 			s = sr.save(subject);
 		}
 		return s;
