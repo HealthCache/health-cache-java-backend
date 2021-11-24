45bdb138f8a9b605047db82d6e1f28f1640feb4b
Eric Kristiansen <Eric.kristiansen@mail.com>
2021-11-24 08:27:31 -0800

    Update ClaimController.java

--- FileClaimService/src/main/java/healthcache/controller/ClaimController.java
+++ FileClaimService/src/main/java/healthcache/controller/ClaimController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import javax.ws.rs.Produces;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,7 +68,6 @@ public class ClaimController {
 	 * @return all claims or null
 	 */
 	@GetMapping("/all")
-	@Produces("application/json")
 	public ResponseEntity<List<Claim>> getAllClaims(){
 		List<Claim> claims = null;
 		HttpStatus responseStatus = HttpStatus.OK;
