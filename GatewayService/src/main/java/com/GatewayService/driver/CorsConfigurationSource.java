18de11b8513b6f5a774d5a2b1cf7449f3d1e99e7
ericKristiansen <eric.kristiansen@mail.com>
2021-11-25 01:32:49 -0800

    configuration class cors

---
+++ GatewayService/src/main/java/com/GatewayService/driver/CorsConfigurationSource.java
@@ -0,0 +1,28 @@
+package com.GatewayService.driver;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+
+@Configuration
+public class CorsConfigurationSource implements WebFluxConfigurer{
+
+	@Bean 
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() { 
+		CorsConfiguration configuration = new CorsConfiguration(); 
+		configuration.setAllowedOrigins(Arrays.asList("*")); 
+		configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS")); 
+		configuration.setExposedHeaders(Arrays.asList("*")); 
+		configuration.setAllowedHeaders(Arrays.asList("*")); 
+		configuration.setAllowCredentials(true); 
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); 
+		source.registerCorsConfiguration("/**", configuration); return source; 
+	}
+
+}
